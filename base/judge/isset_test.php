d142156919e5d6669e0838a54fcf7d3e2e544990
ian <929566854@qq.com>
2025-09-24 15:34:18 +0800

    test isset

---
+++ base/judge/isset_test.php
@@ -0,0 +1,11 @@
+<?php
+
+$arr = array("one"=>"one", "two"=>"two");
+
+
+echo isset($arr["one"])? "have one".PHP_EOL : "have no one".PHP_EOL;
+echo isset($arr["two"])? "have two".PHP_EOL : "have no two".PHP_EOL;
+echo isset($arr["three"])? "have three".PHP_EOL : "have no three".PHP_EOL;
+
+
+
